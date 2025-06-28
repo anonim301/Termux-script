373c70ff46f7cf88245c9ba0a40c5268cd95cd36
anomin301 <mufidyanuar@gmail.com>
2025-06-28 12:13:07 +0700

    Hapus file yang tidak diperlukan

--- hello.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nama="21"
-echo "Halo, umur saya $nama"
--- internet-check.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-log_file="internet_log.txt"
-ping -c 3 google. com > /dev/null 2>&1
-if [ $? -eq 0 ] ;  then
-    echo "$(date) : ✅ internet aktif" >> $log_file
-else
-    echo "$(date) : ❎ internet offline" >> $log_file
-fi
-echo "Log disimpan di $log_file"

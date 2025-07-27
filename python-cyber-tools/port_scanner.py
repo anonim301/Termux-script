d1a2dd741fa36cd65b2f97d0c596b9f134e96dec
anomin301 <mufidyanuar@gmail.com>
2025-07-27 17:23:12 +0700

    tambah file: port_scanner.py

---
+++ python-cyber-tools/port_scanner.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+from pyfiglet import Figlet
+
+#Warna terminal
+HIJAU = "\033[92m"
+MERAH = "\033[91m"
+RESET = "\033[0m"
+
+# Tampilkan banner
+figlet = Figlet(font='digital')  # kamu bisa coba font lain juga
+print(MERAH + figlet.renderText("Port Scanner") + RESET)
+#variabel
+target = input("Masukkan IP atau domain target: ")
+port_range = input("Masukkan rentang port (contoh: 20-80): ")
+
+#Coba resolve domain ke IP
+try:
+	ip_target = socket.gethostbyname(target)
+except socket.gaierror:
+	print(f"{MERAH}❌ Gagal menyelesaikan nama domain: {target}{RESET}")
+	exit()
+
+#Rentang port
+awal, akhir = map(int, port_range.split("-"))
+
+print(f"\nMemindai {target} ({ip_target}) dari port {awal} sampai {akhir}...\n")
+
+#Fungsi pemindaian
+def pemindai_port(port):
+	try:
+		koneksi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		koneksi.settimeout(0.5)
+		hasil = koneksi.connect_ex((ip_target, port))
+		if hasil == 0:
+			try:
+				service = socket.getservbyport(port)
+			except:
+				service = "Unknown"
+			print(f"{HIJAU}[+] Port {port} TERBUKA ({service}){RESET}")
+			koneksi.close()
+	except:
+		pass
+
+#Multittasking
+threads = []
+for port in range(awal, akhir + 1):
+	t = threading.Thread(target=pemindai_port, args=(port,))
+	threads.append(t)
+	t.start()
+
+for t in threads:
+	t.join()
+
+print(f"\n✅ {HIJAU}Pemindaian selesai!{RESET}")
+
+
